351bcb42aa3ee9580e8198e054789bdc81e1244c
Chris Johnson <chrisltd@gmail.com>
2017-08-16 17:55:06 -0400

    fixed template override issue

--- woocommerce.php
+++ woocommerce.php
@@ -29,26 +29,25 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 			<main class="site-main" id="main">
 
 			<?php 
-				$template_name = '\archive-product.php'; 
+				$template_name = '/archive-product.php'; 
 				$args = array(); 
 				$template_path = ''; 
-				$default_path = untrailingslashit( plugin_dir_path(__FILE__) ) . '\woocommerce';
+				$default_path = untrailingslashit( plugin_dir_path(__FILE__) ) . '/woocommerce';
 
-					if ( is_singular( 'product' ) ) {
+				if ( is_singular( 'product' ) ) {
 
-						woocommerce_content();
+					woocommerce_content();
 
-			//For ANY product archive, Product taxonomy, product search or /shop landing page etc Fetch the template override;
-				} 	elseif ( file_exists( $default_path . $template_name ) )
-					{
+				//For ANY product archive, Product taxonomy, product search or /shop landing page etc Fetch the template override;
+				} 	elseif ( file_exists( $default_path . $template_name ) ) {
 					wc_get_template( $template_name, $args, $template_path, $default_path );
 
-			//If no archive-product.php template exists, default to catchall;
+				//If no archive-product.php template exists, default to catchall;
 				}	else  {
 					woocommerce_content( );
 				}
 
-			;?>
+			?>
 
 			</main><!-- #main -->
 
