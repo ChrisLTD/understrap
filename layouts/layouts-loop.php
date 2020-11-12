577f818bb3728ed79f4b6af6c41047097a4d9433
Chris Johnson <chrisltd@gmail.com>
2020-11-12 17:56:18 -0500

    added if to layouts loop

--- layouts/layouts-loop.php
+++ layouts/layouts-loop.php
@@ -1,6 +1,9 @@
-<?php while(the_flexible_field("page_layouts")): ?>
+<?php if (defined('the_flexible_field')): ?>
 
-  <?php include(locate_template('layouts/layout-' . get_row_layout() . '.php')); ?>
+	<?php while(the_flexible_field("page_layouts")): ?>
 
-<?php endwhile; // end page layout ?>
+		<?php include(locate_template('layouts/layout-' . get_row_layout() . '.php')); ?>
 
+	<?php endwhile; // end page layout ?>
+
+<?php endif; ?>
